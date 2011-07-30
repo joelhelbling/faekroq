b12c0f3c41fbfd0ed34d21bbb9491dc8339fd336
Joel Helbling <joel@joelhelbling.com>
2011-07-30 15:18:27 -0400

    using rijndael encryption

--- lib/faekroq.rb
+++ lib/faekroq.rb
@@ -1,11 +1,39 @@
+gem 'crypt', '1.1.4'   if RUBY_VERSION == '1.8.7'
+gem 'crypt19', '1.2.1' if RUBY_VERSION == '1.9.2'
+
+require 'crypt/rijndael'
 
 module FaekRoq
+  KEY = 'A key 16, 24, or 32 bytes length'*4
+  @@encrypted = nil
+
   class << self
+
     def decrypt
-
+      if @@encrypted
+        crypter.decrypt_string @@encrypted
+      end
     end
-    def encrypt(string)
 
+    def password
+      decrypt
     end
+
+    def encrypt(password)
+      crypter.encrypt_string password
+    end
+
+    def password=(new_password)
+      @@encrypted = encrypt(new_password)
+    end
+
+    def encrypted
+      @@encrypted
+    end
+
+    def crypter
+      @@crypter ||= Crypt::Rijndael.new(KEY)
+    end
+
   end
 end
--- spec/lib/faekroq_spec.rb
+++ spec/lib/faekroq_spec.rb
@@ -10,4 +10,38 @@ describe FaekRoq do
     lambda { FaekRoq.encrypt 'foo' }.should_not raise_error
   end
 
+  it 'accepts a password' do
+    lambda { FaekRoq.password = 'foo' }.should_not raise_error
+  end
+
+  it 'has an .encrypted method' do
+    lambda{ FaekRoq.encrypted }.should_not raise_error
+  end
+
+  it 'has an encrypted form of its password' do
+    password = 'foobar7'
+    FaekRoq.password = password
+    FaekRoq.encrypted.should_not == password
+  end
+
+  it 'can encrypt passwords longer than 16 characters' do
+    plain_text = '12345678901234567'
+    FaekRoq.password = plain_text
+    lambda{ FaekRoq.password = plain_text }.should_not raise_error
+  end
+
+  it 'can decrypt a password' do
+    FaekRoq.password = 'foobar'
+    FaekRoq.decrypt.should == 'foobar'
+  end
+
+  xit 'gets its encrypted password from a file' do
+  end
+
+  xit 'can use a key file' do
+  end
+
+  xit 'prompts to create a encrypted password file if none is found' do
+  end
+
 end
